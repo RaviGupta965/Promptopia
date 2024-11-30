a1c537e8a8bb4d5809df821593af6f1d259783e2
RaviGupta965 <clicktoravi1234@gmail.com>
2024-12-01 00:41:15 +0530

    final try

--- next.config.js
+++ next.config.js
@@ -2,24 +2,19 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // If you still need to use the app directory feature.
-  },
-  serverExternalPackages: ["mongoose"], // Keeping this as is for mongoose.
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
   webpack(config) {
-    // Top-level await experiment
+    // Add alias for @components
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, 'components'),
+    };
+
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    };
-
-    // Set up the alias resolution for @components
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@components': path.resolve(__dirname, 'components'), // Make sure to map @components correctly
     };
 
     return config;
