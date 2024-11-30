e409d4c653d4bb449577f5e6361be0d344f846b0
RaviGupta965 <clicktoravi1234@gmail.com>
2024-11-30 23:48:33 +0530

    next-file-debug

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
+    appDir: true, // This is fine for enabling the app directory.
   },
+  serverExternalPackages: ["mongoose"], // Corrected from `serverComponentsExternalPackages`.
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com'], // No change needed here.
   },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    }
-    return config
-  }
-}
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
