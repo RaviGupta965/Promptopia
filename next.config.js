76f450b14501af4ba09a4d459b3d85ff6260bdea
RaviGupta965 <clicktoravi1234@gmail.com>
2025-01-11 01:03:47 +0530

    builds-configured

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  serverExternalPackages: ["mongoose"],
   webpack(config) {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    };
-    return config;
-  },
-};
+    }
+    return config
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
